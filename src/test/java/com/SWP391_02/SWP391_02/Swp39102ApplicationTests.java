318b9d68d1fc3a221e69d2c0866e4d243b95feb2
DucT4 <ductu0831@gmail.com>
2025-10-07 20:46:40 +0700

    first

---
+++ src/test/java/com/SWP391_02/SWP391_02/Swp39102ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SWP391_02.SWP391_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swp39102ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
